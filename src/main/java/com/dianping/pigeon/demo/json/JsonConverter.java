ff17b66370ac4d61794c5f8cd64303d6b5181c3f
xiang.wu <xiang.wu@dianping.com>
2014-08-20 10:09:26 +0800

    update demo

--- src/main/java/com/dianping/pigeon/demo/json/JsonConverter.java
+++ src/main/java/com/dianping/pigeon/demo/json/JsonConverter.java
@@ -1,18 +1,16 @@
-package com.dianping.pigeon.demo;
-
+package com.dianping.pigeon.demo.json;
 
 import java.util.Date;
 
 import com.dianping.pigeon.remoting.common.codec.json.JacksonSerializer;
 
-public class Test {
+public class JsonConverter {
 
 	public static void main(String[] args) {
 		JacksonSerializer serializer = new JacksonSerializer();
 		Date d = new Date();
 		String str = serializer.serializeObject(d);
 		System.out.println(str);
-		System.out.println(d.getTime());
 	}
 
 }
